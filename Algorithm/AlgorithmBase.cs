d22262619326ccfc7c08dd6ee14f1107c0aea7d2
Roman Karpov <poma.05@mail.ru>
2020-01-13 15:26:45 +0400

    init

---
+++ Algorithm/AlgorithmBase.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Algorithm
+{
+    public class AlgorithmBase<T> where T : IComparable
+    {
+        public List<T> Items { get; set; } = new List<T>();
+
+        protected void Swap(int positionA, int positionB)
+        {
+            if (positionA < Items.Count && positionB < Items.Count)
+            {
+                var temp = Items[positionA];
+                Items[positionA] = Items[positionB];
+                Items[positionB] = temp;
+            }
+        }
+
+        public virtual void Sort()
+        {
+            Items.Sort();
+        }
+    }
+
+}
+
